b8761e74a1b0502562738a204ec33489291ef253
Guevaer Arnaud <guevaer8@gmail.com>
2025-05-05 20:04:03 +0200

    adding colors variables

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -13,19 +13,24 @@ function Home() {
           alt="Arnaud Guevaer"
           className="home__img"
         />
+        <p className="home__status">
+          Status : En recherche d'une alternance pour septembre 2025
+        </p>
       </section>
 
       <section className="home__section2" id="home">
         <p className="home__description">
           Passionnée depuis toujours par les nouvelles technologies et les jeux
           vidéo, j’ai décidé de me reconvertir dans le développement web pour
-          faire de cette passion mon métier. J’ai ainsi intégré la Wild Code
-          School et obtenu mon diplôme de developpeur web et web mobile. Avant
-          cette reconversion, mon parcours a débuté en tant que Gendarme Adjoint
-          Volontaire, une expérience marquante qui m’a inculqué rigueur, esprit
-          de cohésion et sens des responsabilités. Puis j’ai travaillé dans la
-          logistique, où j’ai appris à m’adapter rapidement, à organiser
-          efficacement mon travail et à comprendre les enjeux du e-commerce.
+          faire de cette passion mon métier. <br /> J’ai ainsi intégré la Wild
+          Code School et obtenu mon diplôme de developpeur web et web mobile.{" "}
+          <br />
+          Avant cette reconversion, mon parcours a débuté en tant que Gendarme
+          Adjoint Volontaire, une expérience marquante qui m’a inculqué rigueur,
+          esprit de cohésion et sens des responsabilités. <br /> Puis j’ai
+          travaillé dans la logistique, où j’ai appris à m’adapter rapidement, à
+          organiser efficacement mon travail et à comprendre les enjeux du
+          e-commerce.
         </p>
       </section>
     </>

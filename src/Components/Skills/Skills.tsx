417f59cba11b9ce5a01cfc2e35d0337f8787fd22
Guevaer Arnaud <guevaer8@gmail.com>
2025-05-11 10:21:26 +0200

    adding skills icons

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -1,6 +1,54 @@
 import "./Skills.css";
 
 function Skills() {
+  const skill = [
+    {
+      title: "Javascript",
+      img: "./js.png",
+      category: "front-end",
+    },
+    {
+      title: "React",
+      img: "./react.png",
+      category: "front-end",
+    },
+    {
+      title: "Typescript",
+      img: "./ts.png",
+      category: "front-end",
+    },
+    {
+      title: "Node.js",
+      img: "./node.png",
+      category: "back-end",
+    },
+    {
+      title: "Express",
+      img: "./express.png",
+      category: "back-end",
+    },
+    {
+      title: "SQL",
+      img: "./sql.png",
+      category: "back-end",
+    },
+    {
+      title: "Github",
+      img: "./github2.png",
+      category: "tools",
+    },
+    {
+      title: "Gitlab",
+      img: "./gitlab.png",
+      category: "tools",
+    },
+    {
+      title: "Figma",
+      img: "./figma.png",
+      category: "tools",
+    },
+  ];
+
   return (
     <>
       <section className="skills__section" id="skills">
@@ -9,27 +57,42 @@ function Skills() {
         <article className="skills__article">
           <h3 className="skills__subtitle">Front-end</h3>
           <ul className="skills__list">
-            <li className="skills__item">React</li>
-            <li className="skills__item">JavaScript</li>
-            <li className="skills__item">Typescript</li>
+            {skill
+              .filter((s) => s.category === "front-end")
+              .map((s) => (
+                <li key={s.title} className="skills__item">
+                  <img src={s.img} alt={s.title} className="skills__img" />
+                  {s.title}
+                </li>
+              ))}
           </ul>
         </article>
 
         <article className="skills__article">
           <h3 className="skills__subtitle">Back-end</h3>
           <ul className="skills__list">
-            <li className="skills__item">Node.js</li>
-            <li className="skills__item">Express</li>
-            <li className="skills__item">SQL</li>
+            {skill
+              .filter((s) => s.category === "back-end")
+              .map((s) => (
+                <li key={s.title} className="skills__item">
+                  <img src={s.img} alt={s.title} className="skills__img" />
+                  {s.title}
+                </li>
+              ))}
           </ul>
         </article>
 
         <article className="skills__article">
           <h3 className="skills__subtitle">Outils</h3>
           <ul className="skills__list">
-            <li className="skills__item">Git</li>
-            <li className="skills__item">Gitlab</li>
-            <li className="skills__item">Figma</li>
+            {skill
+              .filter((s) => s.category === "tools")
+              .map((s) => (
+                <li key={s.title} className="skills__item">
+                  <img src={s.img} alt={s.title} className="skills__img" />
+                  {s.title}
+                </li>
+              ))}
           </ul>
         </article>
       </section>

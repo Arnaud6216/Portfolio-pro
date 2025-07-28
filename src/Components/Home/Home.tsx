a283bf0f23703ffe4ba7a8f2cb91f23fe826af76
Guevaer Arnaud <guevaer8@gmail.com>
2025-07-28 20:43:46 +0200

    making name typing animation replaying on click

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,14 +1,29 @@
 import "./Home.css";
 import { useLanguage } from "../../context/LanguageContext";
+import { useState } from "react";
 
 function Home() {
   const { t } = useLanguage();
+  const [replaying, setReplaying] = useState(true);
+
+  const handleClick = () => {
+    setReplaying(false); // supprime l'animation
+    setTimeout(() => {
+      setReplaying(true); // la rejoue après un petit délai
+    }, 10);
+  };
   return (
     <>
       <section className="home__section1" id="profile">
         <h1 className="home__title">
           {t.home.title1} <br />
-          <span className="home__name">{t.home.title2}</span>
+          <span
+            className={`home__name ${replaying ? "replay" : ""}`}
+            onClick={handleClick}
+            onKeyUp={handleClick}
+          >
+            {t.home.title2}
+          </span>
         </h1>
         <h2 className="home__subtitle">{t.home.title3}</h2>
         <img src="./arnaud.jpg" alt="Arnaud Guevaer" className="home__img" />
